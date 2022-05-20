0ae284cfc118b9e23a30489ebd717442133467c8
Aditya Choudhary <adityachoudhary1980@gmail.com>
2022-05-20 22:03:27 +0530

    update

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 const baseUrl="https://daily-blog-backend.herokuapp.com/posts/";
 // const baseUrl = "http://localhost:1337/posts/";
 const Post = () => {
@@ -30,7 +31,7 @@ const Post = () => {
             {(blog.image.length!==0)?<img src={blog.image[0].base64} alt="post-image" />:<img src='https://icon-library.com/images/img-icon/img-icon-0.jpg'  alt='post-image'/>}
 
             </div>
-            <h1>{blog.title}</h1>
+            <h1><FontAwesomeIcon icon={faCaretRight} /> {blog.title}</h1>
             <span className="addDate" style={{display:(blog.author===""||blog.author_id==="")?"none":"inherits"}}>posted <span style={{display:(blog.author==="")?"":"inline"}}>by </span> <h5 style={{display:(blog.author==="")?"":"inline"}}><NavLink to={`/author/${blog.author_id}`} style={{color:"black"}}>{blog.author}</NavLink></h5> on {blog.addDate}</span>
 
             <p style={{whiteSpace:"pre-line"}}>{blog.content}</p>
